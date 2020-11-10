deb327085bf875810ec11d828125178acae530f3
Alex Tseng <636479@aacps.org>
2020-11-10 16:34:58 +0000

    Finished maybe?

--- src/main/java/PalindromicNumberFinder.java
+++ src/main/java/PalindromicNumberFinder.java
@@ -13,21 +13,51 @@ public class PalindromicNumberFinder {
 
     // this method should find the next palindromic number
     public int searchForPalindromicNum(int num){
-
+        while(true) {
+          if(Integer.parseInt(reverseNum(num)) == num) {
+            return num;
+          } else {
+            System.out.println(num);
+            num++;
+          }
+        }
     }
 
     // this is a helper method for searchForPalindromicNum. It's purpose is to test if a number is actually a palindrome
     public boolean testPalindromicNum(int num){
-
+      if(reverseNum(num).equals(String.valueOf(num))) {
+        return true;
+      } else {
+        return false;
+      }
+      
 
     }
 
 
     // this is a helper method for testPalindromicNum. It should reverse the number and return it.
     public String reverseNum(int num){
+      // int reversed = 0; 
+      //   while(num > 0) 
+      //   { 
+      //       reversed = reversed * 10 + num % 10; 
+      //       num = num / 10; 
+      //   } 
+      //   return String.valueOf(reversed); 
 
+      // String reversed = "";
+      // String number = String.valueOf(num);
+      // for(int i = number.length(); i > 1; i--) {
+      //   reversed = reversed + number.charAt(i);
+      //   System.out.println(reversed);
+      // }
+      // return reversed;
 
-
+      String reversed = "";
+      for(int i = s.length() - 1; i >= 0; i--) {
+        reversed = reversed + s.charAt(i);
+      } 
+      return String.valueOf(reversed);
     }
 
 
--- src/main/java/PalindromicRunner.java
+++ src/main/java/PalindromicRunner.java
@@ -1,6 +1,9 @@
 public class PalindromicRunner {
     public static void main(String[] args) {
         // be sure you're testing each method as you work then produce your final output using the toString method
+      PalindromicNumberFinder num = new PalindromicNumberFinder(10);
+
+      System.out.println(num.searchForPalindromicNum(1665531323));
 
     }
 }
